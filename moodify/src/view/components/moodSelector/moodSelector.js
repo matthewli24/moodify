814bfde80937e0f56918926f0d85a2564634e9d4
Matthew Li <matthewli2491@gmail.com>
2018-10-29 18:44:57 -0400

    matt's first commit

---
+++ moodify/src/view/components/moodSelector/moodSelector.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './moodSelector.css';
+
+class MoodSelector extends Component {
+    render() {
+      return (
+        <div className="wrapper">
+            <div>MoodSelector</div>
+        </div>
+      );
+    }
+  }
+  
+  export default MoodSelector;
