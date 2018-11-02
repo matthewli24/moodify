fbdd8da4d4813641e3938206ef15ebaa873f624c
Matthew Li <matthewli2491@gmail.com>
2018-11-02 13:22:13 -0400

    topbar

--- moodify/src/view/components/topbar/topbar.js
+++ moodify/src/view/components/topbar/topbar.js
@@ -5,7 +5,18 @@ class Topbar extends Component {
     render() {
       return (
         <div className="wrapper">
-            <div>Moodify</div>
+            <div className="logo">
+              <a>Moodify</a>
+            </div>
+
+            <div className="message">
+              <a>How Are You Feeling Beautiful?</a>
+            </div>
+
+            <div className="userLog">
+              <a>Log In</a>
+              <a>Sign Up</a>
+            </div>
         </div>
       );
     }
