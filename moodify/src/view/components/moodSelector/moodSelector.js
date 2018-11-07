111e8b72b1aea5958e4f37369c149ffe74e58aaf
Matthew Li <matthewli2491@gmail.com>
2018-11-07 13:54:59 -0500

    added energybar

--- moodify/src/view/components/moodSelector/moodSelector.js
+++ moodify/src/view/components/moodSelector/moodSelector.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './moodSelector.css';
 
 class MoodSelector extends Component {
-    render() {
-      return (
-        <div className="selectorWrapper">
-            <div className="angry"></div>
-            <div className="sad"></div>
-            <div className="neutral"></div>
-            <div className="happy"></div>
-            <div className="beautiful"></div>
-            <div className="box"></div>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="selectorWrapper">
+          <div className="angry"></div>
+          <div className="sad"></div>
+          <div className="neutral"></div>
+          <div className="happy"></div>
+          <div className="beautiful"></div>
+          <div className="box"></div>
+      </div>
+    );
   }
-  
-  export default MoodSelector;
+}
+
+export default MoodSelector;
