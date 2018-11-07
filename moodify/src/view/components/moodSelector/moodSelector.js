de2389018ede697a95c04ace79d5bf872faf3ccc
Matthew Li <matthewli2491@gmail.com>
2018-11-07 15:06:13 -0500

    fix state of mood

--- moodify/src/view/components/moodSelector/moodSelector.js
+++ moodify/src/view/components/moodSelector/moodSelector.js
@@ -2,15 +2,52 @@ import React, { Component } from 'react';
 import './moodSelector.css';
 
 class MoodSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mood: "neutral"
+    }
+  }
+
+  selectAngry = () => {
+    this.setState({
+      mood: "angry"
+    })
+  };
+
+  selectSad = () => {
+    this.setState({
+      mood: "sad"
+    })
+  };
+
+  selectNeutral = () => {
+    this.setState({
+      mood: "neutral"
+    })
+  };
+
+  selectHappy = () => {
+    this.setState({
+      mood: "happy"
+    })
+  };
+
+  selectBeautiful = () => {
+    this.setState({
+      mood: "beautiful"
+    })
+  };
+  
+
   render() {
     return (
       <div className="selectorWrapper">
-          <div className="angry"></div>
-          <div className="sad"></div>
-          <div className="neutral"></div>
-          <div className="happy"></div>
-          <div className="beautiful"></div>
-          <div className="box"></div>
+          <div className="angry" onClick={this.selectAngry}></div>
+          <div className="sad" onClick={this.selectSad}></div>
+          <div className="neutral" onClick={this.selectNeutral}></div>
+          <div className="happy" onClick={this.selectHappy}></div>
+          <div className="beautiful" onClick={this.selectBeautiful}></div>
       </div>
     );
   }
