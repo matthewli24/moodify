4264e878409d37e51ba707275a808edc81902b1e
Silvena Chan <silvenac@users.noreply.github.com>
2018-11-08 16:48:03 -0500

    authentication stubs

--- react-backend/app.js
+++ react-backend/app.js
@@ -4,14 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth-routes');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +40,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.set('port', (process.env.PORT || 3000));
+
+models.sequelize.sync({force: false})
+  .then(() => {
+    const port = app.get('port');
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  });
 
 module.exports = app;
