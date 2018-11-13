ccc01c630dc82d8df29863e8b21bd4d3af1ca42d
Matthew Li <matthewli2491@gmail.com>
2018-11-13 12:30:20 -0500

    stuff

--- react-backend/app.js
+++ react-backend/app.js
@@ -61,7 +61,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8000));
 // const port = app.get('port');
 // app.listen(port, () => {
 //   console.log(`Listening on port ${port}`);
