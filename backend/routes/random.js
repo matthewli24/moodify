9af290aa99e8f77c8265284d78cbba967461b2dd
Silvena Chan <silvenac@users.noreply.github.com>
2018-12-12 18:02:01 -0500

    stub functions for spotify api

--- backend/routes/random.js
+++ backend/routes/random.js
@@ -29,7 +29,7 @@ function getToken(callback) {
     });
 }
 
-function getTracks(playlistId, callback, access_token, retry=3) {
+function getTracks(playlistId, access_token, callback, retry=3) {
     let options = {
         url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
         headers: { 'Authorization': 'Bearer ' + access_token },
@@ -40,18 +40,88 @@ function getTracks(playlistId, callback, access_token, retry=3) {
         const status = response.statusCode;
         if (!error && (status === 200 || status === 201)) {
             let trackIds = new Set();
-            for (let item of body.items) {
+            for (const item of body.items) {
                 trackIds.add(item.track.id);
             }
             callback(Array.from(trackIds));
         }
         else if (error && retry > 0) {
-            getTracks(playlistId, callback, retry-1);
+            getTracks(playlistId, access_token, callback, retry-1);
         }
     });
 }
 
+function getAudioFeatures(trackIds, access_token, callback, retry=3) {
+    const query = trackIds.join();
+    let options = {
+        url: `https://api.spotify.com/v1/audio-features/?ids=${query}`,
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        json: true
+    };
 
+    request.get(options, (error, response, body) => {
+        const status = response.statusCode;
+        if (!error && (status === 200 || status === 201)) {
+            const audioFeatures = body.audio_features;
+            let features = new Array();
+            for (let feat of audioFeatures) {
+                const audioFeature = {
+                    id: feat.id,
+                    danceability: feat.danceability,
+                    energy: feat.energy,
+                    key: feat.key,
+                    loudness: feat.loudness,
+                    mode: feat.mode,
+                    speechiness: feat.speechiness,
+                    acousticness: feat.acousticness,
+                    instrumentalness: feat.instrumentalness,
+                    liveness: feat.liveness,
+                    valence: feat.valence,
+                    tempo: feat.tempo
+                }
+                features.push(audioFeature);
+            }
+            callback(features);
+        }
+        else if (error && retry > 0) {
+            getAudioFeatures(trackIds, access_token, callback, retry - 1);
+        }
+        else {
+            console.log(error);
+        }
+    });
+}
+var tracks = ['2VjtYe7gpfUi2OkGxR2O2z','4yweVeiVhklj00DRKcQi9z']
+
+//filterBy should be object { property: conditionFunc }
+function filterFeatures(audioFeatures, filterBy) {
+    const filtered = audioFeatures.filter( track => {
+        const features = Objects.keys(track);
+        const conditionFunc = filterBy[feature];
+        return conditionFunc(featureVal);
+    });
+    return filtered;
+}
+
+var filter = {
+    energy: (val) => {val > 0.7 && val < 0.9}
+}
+
+function filterByEnergy(playlistId, energy, access_token) {
+    getTracks(playlistId, access_token, tracks)
+}
+
+getToken(access_token => {
+    getTracks("37i9dQZF1DXaK0O81Xtkis", access_token, tracks => {
+        let token = access_token;
+        // console.log(token);
+
+        // getAudioFeatures (tracks, token, features => {
+        //     console.log(token);
+        //     // console.log(features);
+        // });
+    });
+})
 
 function refresh(refresh_token, callback) {
     //use the token received in the `callback` function
