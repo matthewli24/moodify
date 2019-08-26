0155d80eb03b687a7849b5a5e6a07965e0198884
Matthew Li <matthewli2491@gmail.com>
2019-08-26 15:17:58 -0400

    adjusted BG and btn and bar css

--- client/src/components/submitBtn/submitBtn.js
+++ client/src/components/submitBtn/submitBtn.js
@@ -14,7 +14,8 @@ class SubmitBtn extends Component {
     let energy = this.props.energy / 100;
 
     console.log(mood, energy)
-    if (mood !== null) {
+    if (mood != null) {
+      console.log("mood is not null")
       // fetch(`/spotify/playlist?mood=${mood}&energy=${energy}`)
       //   .then(res => res.json())
       //   .then(res => {
@@ -27,6 +28,7 @@ class SubmitBtn extends Component {
       //   .catch((err) => {
       //     console.log("error fetching playlist: ", err)
       //   })
+
     } else {
       alert('PLEASE SELECT A MOOD!');
     }
